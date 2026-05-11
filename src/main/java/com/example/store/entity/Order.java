86206fdc9af7e993bfc8768287e3218cb1f76d38
ishara <ishara9@gmail.com>
2026-05-11 18:18:17 +1200

    Initial Commit from zip

---
+++ src/main/java/com/example/store/entity/Order.java
@@ -0,0 +1,20 @@
+package com.example.store.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "\"order\"")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+}
